5f094aabe3abfb2029f54a7d460720693cd55114
Nicklas Pouey-Winger <nicklas.pouey-winger@evry.com>
2017-11-10 13:19:53 +0100

    Query DB for stores and dump to view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,3 +17,9 @@ exports.createStore = async (req, res) => {
   );
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
+};
