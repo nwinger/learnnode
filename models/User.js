8a8ea6c37f6d4adb185d69e81582608881a66470
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-01-30 18:19:04 +0100

    Creating user accounts and validation on register

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import { map } from "bluebird";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require("md5");
+const validator = require("validator");
+const mongoDbErrorHandler = require("mongoose-mongodb-errors");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		validate: [validator.isEmail, "Invalid Email Address"],
+		required: "Please supply an email address"
+	},
+	name: {
+		type: String,
+		required: "Please supply a name",
+		trim: true
+	}
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+userSchema.plugin(mongoDbErrorHandler);
+
+module.exports = mongoose.model("User", userSchema);
