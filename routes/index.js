689cee17c543e7598503dd57b5b23256c438f0de
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-02-03 10:32:14 +0100

    Edit user account

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,8 @@ router.post(
 );
 
 router.get("/logout", authController.logout);
+
+router.get("/account", authController.isLoggedIn, userController.account);
+router.post("/account", catchErrors(userController.updateAccount));
+
 module.exports = router;
