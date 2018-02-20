130bc80a40e76f3eb23ba06c55da12c4b24f83e4
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-02-20 21:01:00 +0100

    Basic user permissions

--- models/Store.js
+++ models/Store.js
@@ -1,66 +1,71 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const slug = require("slugs");
+const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: "Please enter a store name!"
-	},
-	slug: String,
-	description: {
-		type: String,
-		trim: true
-	},
-	tags: [String],
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	location: {
-		type: {
-			type: String,
-			default: "Point"
-		},
-		coordinates: [
-			{
-				type: Number,
-				required: "You must supply coordinates!"
-			}
-		],
-		address: {
-			type: String,
-			required: "You must supply an address!"
-		}
-	},
-	photo: String
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [
+      {
+        type: Number,
+        required: 'You must supply coordinates!'
+      }
+    ],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  },
+  photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    require: 'You must supply an author'
+  }
 });
 
-storeSchema.pre("save", async function(next) {
-	if (!this.isModified("name")) {
-		next(); // Skip it
-		return; // Stop this function from running
-		// TODO: make more resiliant so slugs are unique
-	}
-	this.slug = slug(this.name);
+storeSchema.pre('save', async function(next) {
+  if (!this.isModified('name')) {
+    next(); // Skip it
+    return; // Stop this function from running
+    // TODO: make more resiliant so slugs are unique
+  }
+  this.slug = slug(this.name);
 
-	// find other stores that have a similar slug of wes, wes-1, wes-2
-	const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, "i");
-	const storesWithSlugs = await this.constructor.find({ slug: slugRegEx });
-	if (storesWithSlugs.length) {
-		this.slug = `${this.slug}-${storesWithSlugs.length + 1}`;
-	}
+  // find other stores that have a similar slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlugs = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlugs.length) {
+    this.slug = `${this.slug}-${storesWithSlugs.length + 1}`;
+  }
 
-	next();
+  next();
 });
 
 storeSchema.statics.getTagsList = function() {
-	return this.aggregate([
-		{ $unwind: "$tags" },
-		{ $group: { _id: "$tags", count: { $sum: 1 } } },
-		{ $sort: { count: -1 } }
-	]);
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
 };
 
-module.exports = mongoose.model("Store", storeSchema);
+module.exports = mongoose.model('Store', storeSchema);
